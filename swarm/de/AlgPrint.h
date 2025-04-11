9088427cde207b8d1d5d7a798870ec4931662d07
Liuming9124 <a723479.a12@gmail.com>
2025-04-12 00:09:45 +0800

    lshade

---
+++ swarm/de/AlgPrint.h
@@ -0,0 +1,100 @@
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+using namespace std;
+using std::setw;
+
+class AlgPrint{
+public:
+    AlgPrint(){}
+    AlgPrint( int run, const string& folder, const string& alg) : _run(run), _folder(folder), _alg(alg){
+        _lastPrint = -1;
+        _maxPrint = _lastPrint;
+    }
+    
+    void clearResult(const std::string& filename) {
+        std::ofstream ofs(filename, std::ofstream::out | std::ofstream::trunc);
+    }
+
+    double calculateMean(const std::vector<double>& numbers) {
+        if (numbers.empty()) return 0.0;
+        double sum = 0.0;
+        for(double num : numbers) {
+            sum += num;
+        }
+        return sum / numbers.size();
+    }
+
+    template<typename T>
+    void NewShowData(vector<vector<T>>& data, int amount){
+        data.resize(_run);
+        for (int i = 0; i < _run; i++){
+            data[i].resize(amount);
+        }
+        cout << "Data size: " << data.size() << " " << data[0].size() << endl;
+    }
+
+    template<typename T>
+    void SetData(int run, vector<vector<T>>& data, T num, int iter){
+        if (_lastPrint==-1){
+            for (int i=0; i <= iter; i++){
+                data[run][i] = num;
+            }
+            _lastPrint = iter;
+        }
+        else {
+            if (_lastPrint==iter){
+                data[run][iter] = num;
+            }
+            else{
+                for (int i = _lastPrint+1; i <= iter; i++){
+                    data[run][i] = num;
+                }
+                _lastPrint = iter;
+            }
+        }
+        if (_lastPrint != _maxPrint)
+            _maxPrint = _lastPrint;
+    }
+
+    template<typename T>
+    void PrintToFile(string fileName, vector<vector<T>>& data, int iter){
+        // clear old file
+        clearResult(fileName);
+        // write to file
+        ofstream file(fileName, ios_base::app);
+        file << scientific << setprecision(8);
+        if (file.is_open()) {
+            for (int i=0; i<_run; i++){
+                file << data[i][0] << endl;
+            }
+        }
+        else {
+            cerr << "Unable to open file!\n";
+        }
+    }
+
+    void NewShowDataInt(int amount)    { NewShowData(_dataInt, amount); }
+    void NewShowDataDouble(int amount) { NewShowData(_dataDouble, amount); }
+    void SetDataInt(int run, long int num, int iter)       { SetData(run, _dataInt, num, iter); }
+    void SetDataDouble(int run, long double num, int iter) { SetData(run, _dataDouble, num, iter); }
+    void PrintToFileInt(const std::string& fileName, int iter)       { PrintToFile(fileName, _dataInt, iter); }
+    void PrintToFileDouble(const std::string& fileName, int iter)    { PrintToFile(fileName, _dataDouble, iter); }
+
+    void init() {
+        _lastPrint = -1;
+        _maxPrint = _lastPrint;
+    }
+
+private:
+    string _folder;
+    string _alg;
+    int _run;
+    int _lastPrint;
+    int _maxPrint;
+    vector<vector<long int>> _dataInt;
+    vector<vector<long double>> _dataDouble;
+};
---
+++ swarm/main_lshade.cpp
@@ -0,0 +1,28 @@
+#include "lshade.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int Run, Func, NP, FESS, Dim, Arch, H, NPmin;
+
+    stringstream ss;
+    for (int i = 1; i < argc; i++)
+        ss << argv[i] << " ";
+    ss >> Run;
+    ss >> Func;
+    ss >> NP;
+    // ss >> FESS;
+    ss >> Dim;
+    ss >> Arch;
+    ss >> H;
+    ss >> NPmin;
+    
+    FESS = Dim * 10000;
+    
+    Lshade lshade;
+    lshade.RunALG(Run, Func, NP, FESS, Dim, Arch, H, NPmin);
+    return 0;
+}
